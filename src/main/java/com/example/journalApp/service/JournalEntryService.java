ebd5e4e136c685248578e2557d00f48680ce06a2
Nachiketa1097 <nachiketa1097@gmail.com>
2024-12-29 20:43:20 +0530

    transaction management

--- src/main/java/com/example/journalApp/service/JournalEntryService.java
+++ src/main/java/com/example/journalApp/service/JournalEntryService.java
@@ -6,6 +6,7 @@ import com.example.journalApp.repository.JournalEntryRepository;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -19,11 +20,17 @@ public class JournalEntryService {
     @Autowired
     private UserService userService;
 
+    @Transactional // This annotation means if any line will not execute in the method it will roll back all code
     public void saveEntry(JournalEntry journalEntry, String userName) {
-        User user = userService.findByUserName(userName);
-        JournalEntry saved = journalEntryRepository.save(journalEntry);
-        user.getJournalEntries().add(saved);
-        userService.saveEntry(user);
+      try {
+          User user = userService.findByUserName(userName);
+          JournalEntry saved = journalEntryRepository.save(journalEntry);
+          user.getJournalEntries().add(saved);
+          user.setUserName(null);// by doing this it will save data in journalEntry but not in user that's why we need @Transactional
+          userService.saveEntry(user);
+      }catch (Exception e){
+          System.out.println("Save entry" + e);
+      }
     }
 
     public void saveEntry(JournalEntry journalEntry) {
